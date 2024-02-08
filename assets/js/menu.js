ad1fbf8a6dbfcf195bb35723d241f3fb2d6ff646
Hardilles Germano <hardilles.germano@gmail.com>
2024-02-08 14:56:20 -0300

    nav

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -8,3 +8,18 @@ function menuShow(){
        
     }
 }
+
+//Pintar o item selecionado no menu//
+
+const menuItem = document.querySelectorAll('.nav-item');
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
